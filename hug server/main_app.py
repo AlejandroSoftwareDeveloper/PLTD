0bff59bccf15ebb82d0c5de5b7ad135393f5e17b
AlejandroSoftwareDeveloper <46248527+AlejandroSoftwareDeveloper@users.noreply.github.com>
2024-01-26 04:26:20 +0100

    Delete hug server directory

--- hug server/main_app.py
+++
@@ -1,9 +0,0 @@
-import requests
-import uuid
-import datetime
-
-
-myuuid = str(uuid.uuid1()).split('-')[4]
-
-data = requests.get("http://localhost:8000/validar?uuid=" + myuuid)  
-print(data.json())
