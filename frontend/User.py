e297be8d67ba768bebe68804bb1dab33a562cf5f
dark850916 <dark850916@gmail.com>
2024-01-29 18:09:13 +0100

    Cambios pendientes en esta rama

--- frontend/User.py
+++ frontend/User.py
@@ -22,12 +22,8 @@ class Usuario:
     def get_user(self):
         return {
             "uid": self.uid,
-            "initial_date_of_test": str(self.fipru)
-            if self.fipru is not None
-            else self.fipru,
-            "initial_date_of_premiun": str(self.fipag)
-            if self.fipag is not None
-            else self.fipag,
+            "initial_date_of_test": str(self.fipru) or self.fipru,
+            "initial_date_of_premiun": str(self.fipag) or self.fipag,
             "days_of_test": self.diaspru,
             "days_of_use_as_premiun": self.diaspag,
             "has_temporal_licence_due": self.terminopru,
@@ -38,36 +34,30 @@ class Usuario:
         return {
             "uid": self.uid,
             "initial_date_of_test": datetime.datetime.now(),
-            "initial_date_of_premiun": str(self.fipag)
-            if self.fipag is not None
-            else self.fipag,
+            "initial_date_of_premiun": str(self.fipag) or self.fipag,
             "days_of_test": self.diaspru,
             "days_of_use_as_premiun": self.diaspag,
             "has_temporal_licence_due": self.terminopru,
             "has_payment_license_due": self.terminopag,
         }
 
-
-def registrar_usuario():
-    return Usuario()
- 
-async def tiempo_restante(data,string):
-    year,month, days = data[string].split("-")
-    test_time = datetime.datetime(int(year),int(month),int(days)) 
-    today =  datetime.datetime.now()
-    return [test_time,today]
-
-async def user_has_tests_days(data:dict):
-    test_time,today = await tiempo_restante(data,'start_date_for_test')
-    return int((today - test_time).days) <= data['days_of_test'] and data['tests_days_selected'] 
+    async def tiempo_restante(self,cond='test'):
+        year, month, days = (self.initial_date_of_premiun if cond == 'premiun' else self.initial_date_of_test).split("-")
+        test_time = datetime.datetime(int(year),int(month),int(days)) 
+        today =  datetime.datetime.now()
+        return int((today - test_time).days)
     
-async def user_has_premiun_days(data:dict):
-    test_time,today = await tiempo_restante(data,'start_date_for_premiun_use')
-    return int((today - test_time).days) <= data['days_of_use_as_premiun'] and data['premiun_days_selected']
 
-async def dias_restantes(data,string):
-    test_time,today = await tiempo_restante(data,string)
-    return int((today - test_time).days)
+    # async def user_has_tests_days(self,data:dict):
+    #     return await self.tiempo_restante(data) <= data['days_of_test'] and data['tests_days_selected'] 
+        
+    # async def user_has_premiun_days(self,data:dict):
+    #     test_time,today = await self.tiempo_restante(data,'start_date_for_premiun_use')
+    #     return int((today - test_time).days) <= data['days_of_use_as_premiun'] and data['premiun_days_selected']
+
+    # async def dias_restantes(self,data,string):
+    #     test_time,today = await self.tiempo_restante(data,string)
+    #     return int((today - test_time).days)
 
     # print((today - test_time) >= data['days_of_test'] )
     # time_delta = datetime.timedelta(days=data['days_of_test'])
--- frontend/ctk_main.py
+++ frontend/ctk_main.py
@@ -24,11 +24,13 @@ data = {
 }
 
 usr = Usuario()
-myuuid = usr.get_user()
-miid = myuuid["uid"]
-mydicttest = {"uid": miid,"start_date_for_test":datetime.datetime.now(),"tests_days_selected":True}
-mydictpremiun = {"uid": miid,"start_date_for_premiun_use":datetime.datetime.now(),"premiun_days_selected":True }
-mi_info = []
+user = usr.get_user()
+miid = user["uid"]
+# mydicttest = {"uid": miid,"start_date_for_test":datetime.datetime.now(),"tests_days_selected":True}
+# mydictpremiun = {"uid": miid,"start_date_for_premiun_use":datetime.datetime.now(),"premiun_days_selected":True }
+# mi_info = []
+conexion = False
+
 
 async def fetch_status(url:str)-> dict:
     response :Response = await asyncio.to_thread(requests.get,url,None)
@@ -36,39 +38,43 @@ async def fetch_status(url:str)-> dict:
 
 
 async def main():
-    get_data: Task[dict] = asyncio.create_task(fetch_status("http://localhost:8000/userprofile/userprofile"))
-    get_data: dict = await get_data
-    if len(get_data['response'].json()) > 0:
-        global mi_info,data
-        mi_info.append(get_data['response'].json())
-        mi_info = [elm for elm in mi_info[0] if elm['uid'] == str(miid)][0]
-        data['is_test_user'] = await user_has_tests_days(mi_info)
-        data['is_user'] = await user_has_premiun_days(mi_info)
-        data['dias_restantes_de_prueba'] = await dias_restantes(mi_info,'start_date_for_test')
-        data['dias_restantes_de_premiun'] = await dias_restantes(mi_info,'start_date_for_premiun_use')
+    try:
+        get_data: Task[dict] = asyncio.create_task(fetch_status("http://localhost:8000/userprofile/userprofile"))
+        get_data: dict = await get_data
+        global conexion 
+        conexion = True
+    except:
+        print("No se pudo realizar la conexion.")
+    # if len(get_data['response'].json()) > 0:
+    #     global mi_info,data
+    #     mi_info.append(get_data['response'].json())
+    #     mi_info = [elm for elm in mi_info[0] if elm['uid'] == str(miid)][0]
+        # data['is_test_user'] = await user_has_tests_days(mi_info)
+        # data['is_user'] = await user_has_premiun_days(mi_info)
+        # data['dias_restantes_de_prueba'] = await dias_restantes(mi_info,'start_date_for_test')
+        # data['dias_restantes_de_premiun'] = await dias_restantes(mi_info,'start_date_for_premiun_use')
 
 asyncio.run(main=main())
 
+# def register_test_user():
+#     global mi_info
+#     if len(mi_info) == 0:
+#         newdata = requests.post(data["url"] + "userprofile/userprofile/",data=mydicttest)
+#         mi_info = newdata.json()
+#         label_time_to_complete.configure(text=data['test-user-message']) 
+#         btn_test.pack_forget()
 
-def register_test_user():
-    global mi_info
-    if len(mi_info) == 0:
-        newdata = requests.post(data["url"] + "userprofile/userprofile/",data=mydicttest)
-        mi_info = newdata.json()
-        label_time_to_complete.configure(text=data['test-user-message']) 
-        btn_test.pack_forget()
-
-def register_premiun_user():
-    global mi_info
-    if len(mi_info) == 0:
-        newdata = requests.post(data["url"] + "userprofile/userprofile/",data=mydictpremiun)
-        mi_info = newdata.json()
-    else:
-        newdata = requests.put(data["url"] + "userprofile/userprofile/" + str(mi_info['id']), data=mydictpremiun)
-        mi_info = newdata.json()
-    if btn_test:
-        btn_test.pack_forget()
-    label_time_to_complete.configure(text="Tiene 30 dias como usuario oficial.") 
+# def register_premiun_user():
+#     global mi_info
+#     if len(mi_info) == 0:
+#         newdata = requests.post(data["url"] + "userprofile/userprofile/",data=mydictpremiun)
+#         mi_info = newdata.json()
+#     else:
+#         newdata = requests.put(data["url"] + "userprofile/userprofile/" + str(mi_info['id']), data=mydictpremiun)
+#         mi_info = newdata.json()
+#     if btn_test:
+#         btn_test.pack_forget()
+#     label_time_to_complete.configure(text="Tiene 30 dias como usuario oficial.") 
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -77,31 +83,41 @@ root.geometry("500x350")
 
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
-
 label = customtkinter.CTkLabel(
     master=frame, text=data["label-title"], font=("Roboto", 24)
 )
 label.pack(pady=20, padx=10)
-label_time_to_complete = customtkinter.CTkLabel(
-    master=frame, text=data["first-time-message"] 
-    if not data['is_test_user'] or not data['is_user'] else 
-    "Le restan {} dias de prueba".format(data['dias_restantes_de_prueba']) 
-    if data['is_test_user'] and not data['is_user'] else 
-    "Le restan {} dias de uso".format(data['dias_restantes_de_premiun']) if data['is_user'] else 
-    "", font=("Roboto", 12)
-)
-label_time_to_complete.pack(pady=12, padx=10)
-if not data["is_test_user"]:
+
+if not conexion:
+    label = customtkinter.CTkLabel(
+        master=frame, text="No es posible conectarse al servicio.\n revise su conexion a internet \n o contacte con el administrador del servicio.",
+        font=("Roboto", 12)
+    )
+    label.pack(pady=30, padx=10)
+    
+else:
+    label_time_to_complete = customtkinter.CTkLabel(
+        master=frame, text=data["first-time-message"] 
+        if not data['is_test_user'] or not data['is_user'] else 
+        "Le restan {} dias de prueba".format(data['dias_restantes_de_prueba']) 
+        if data['is_test_user'] and not data['is_user'] else 
+        "Le restan {} dias de uso".format(data['dias_restantes_de_premiun']) if data['is_user'] else 
+        ""
+        , font=("Roboto", 12)
+    )
+    label_time_to_complete.pack(pady=12, padx=10)
+    # if not data["is_test_user"]:
     btn_test = customtkinter.CTkButton(
-        master=frame, text=data["btn-test"], command=register_test_user
+        master=frame, text="Probar aplicacion",
+        # command=register_test_user
     )
     btn_test.pack(pady=12, padx=10)
-button1 = customtkinter.CTkButton(
-    master=frame, text=data["btn-buy"], 
-    command=register_premiun_user
-)
+    button1 = customtkinter.CTkButton(
+        master=frame, text="Comprar aplicacion", 
+        # command=register_premiun_user
+    )
 
-button1.pack(pady=12, padx=10)
-label.pack(pady=12, padx=10)
+    button1.pack(pady=12, padx=10)
+    label.pack(pady=12, padx=10)
 
 root.mainloop()
